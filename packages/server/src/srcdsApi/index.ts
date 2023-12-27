641ffebaabaabef425a1cf15e8aa14f1c916e8d5
Tripperful <tripperful@gmail.com>
2023-12-27 16:08:41 +0100

    Fix logging

--- packages/server/src/srcdsApi/index.ts
+++ packages/server/src/srcdsApi/index.ts
@@ -1,14 +1,21 @@
+import { dbgInfo } from 'src/util';
 import { RconSrcdsApi } from './RconSrcdsApi';
 import { SrcdsApi } from './SrcdsApi';
 
-let api: SrcdsApi;
+const srcdsApis: Record<string, SrcdsApi> = {};
 
 export const getSrcdsApi: (ip: string, port: number) => SrcdsApi = (
   ip,
   port,
 ) => {
+  const apiHost = ip + ':' + port;
+  let api = srcdsApis[apiHost];
+
   if (!api) {
+    dbgInfo(`Creating a new SRCDS API instance for ${apiHost}`);
+
     api = new RconSrcdsApi(ip, port);
+    srcdsApis[apiHost] = api;
   }
 
   return api;
