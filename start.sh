f7bf617036ff3319dd33104e55b49d9685db3100
Tripperful <tripperful@gmail.com>
2024-01-01 22:45:34 +0100

    Automatically fetch remote and rebuild/restart on changes

--- start.sh
+++ start.sh
@@ -10,21 +10,26 @@ if [ ! -d "/motd-menu" ]; then
 fi
 
 cd /motd-menu
+cp ./start.sh /start.sh
 
-git fetch
+node ./packages/server/dist/index.js &
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+while :; do
+  sleep 10
+  git fetch
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "Git repo is up-to-date"
-elif [ $LOCAL = $BASE ]; then
+  UPSTREAM=${1:-'@{u}'}
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse "$UPSTREAM")
+  BASE=$(git merge-base @ "$UPSTREAM")
+
+  if [ $LOCAL != $REMOTE ]; then
     echo "Remote changed, pulling changes and re-building"
-    git pull
+    git reset --hard origin/master
     npm ci
     npm run build
-fi
-
-node ./packages/server/dist/index.js
+    echo "Restarting node.js server"
+    pkill node
+    node ./packages/server/dist/index.js &
+  fi
+done
