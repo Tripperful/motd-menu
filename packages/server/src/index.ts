9f77a7045e651e6aa9b38e5e8d20ec23f53dd405
Tripperful <tripperful@gmail.com>
2024-01-25 19:59:37 +0100

    Add a test WS server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,13 +3,14 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import expressStaticGzip from 'express-static-gzip';
 import path from 'path';
+import { WebSocketServer } from 'ws';
 import { config } from '~root/config';
 import { api } from './api';
 import { authMiddleware } from './auth';
 import { db } from './db';
-import { logDbgInfo } from './util';
-import { JsonUdp } from './udp';
 import { MockUdpServer } from './mock/MockUdpServer';
+import { JsonUdp } from './udp';
+import { logDbgInfo } from './util';
 
 const app = express();
 
@@ -40,9 +41,37 @@ export const jsonUdp = new JsonUdp(
 db.init().then(() => {
   console.log('Database initialized');
 
-  app.listen(config.port, () => {
+  const server = app.listen(config.port, () => {
     console.log(`MOTD-menu web server listening at port ${config.port}`);
+
+    const wsServer = new WebSocketServer({
+      server,
+    });
+
+    wsServer.on('listening', () => {
+      `WebSocket server listening at port ${config.port}`;
+    });
+
+    wsServer.on('connection', (ws, req) => {
+      const { remoteAddress, remotePort } = req.socket;
+      const remoteHost = `${remoteAddress}:${remotePort}`;
+
+      console.log(`New WS connection: ${remoteHost}`);
+
+      ws.on('message', async (data) => {
+        const txtData = data.toString('utf-8');
+
+        console.log(`Incoming WS message from ${remoteHost}: ${txtData}`);
+
+        await new Promise((res) => setTimeout(res, 1000));
+
+        console.log(`Sending data back to ${remoteHost} after 1 second`);
+
+        ws.send(txtData);
+      });
+    });
   });
+
   jsonUdp.connect();
 
   if (process.env.MOTD_MOCK_UDP_SERVER_PORT) {
