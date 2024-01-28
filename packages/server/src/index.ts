87f7e9bc1030829ca53376eaf15353a384f5e9b4
Tripperful <tripperful@gmail.com>
2024-01-28 22:40:53 +0100

    Handle WS errors (temporary, for debug)

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -60,15 +60,45 @@ db.init().then(() => {
       console.log(`New WS connection: ${remoteHost}`);
 
       ws.on('message', async (data) => {
-        const txtData = data.toString('utf-8');
+        try {
+          const txtData = data.toString('utf-8');
 
-        console.log(`Incoming WS message from ${remoteHost}: ${txtData}`);
+          console.log(`Incoming WS message from ${remoteHost}: ${txtData}`);
 
-        await new Promise((res) => setTimeout(res, 1000));
+          await new Promise((res) => setTimeout(res, 1000));
 
-        console.log(`Sending data back to ${remoteHost} after 1 second`);
+          console.log(`Sending data back to ${remoteHost} after 1 second`);
 
-        ws.send(txtData);
+          ws.send(txtData);
+        } catch (e) {
+          let dataAscii = 'ERROR';
+          let dataUtf8 = 'ERROR';
+          let dataBase64 = 'ERROR';
+
+          try {
+            dataAscii = data.toString('ascii');
+          } catch {}
+
+          try {
+            dataUtf8 = data.toString('utf-8');
+          } catch {}
+
+          try {
+            dataBase64 = data.toString('base64');
+          } catch {}
+
+          ws.send(
+            JSON.stringify({
+              type: 'wsError',
+              error: e,
+              errorMsg: e?.message,
+              errorStack: e?.stack,
+              dataAscii,
+              dataUtf8,
+              dataBase64,
+            }),
+          );
+        }
       });
     });
   });
