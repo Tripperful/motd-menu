77f829c0c54ee713641d9010fa37ab2068b3aa58
Tripperful <tripperful@gmail.com>
2024-08-30 01:35:43 +0200

    Update ranks colors

--- packages/server/src/util/ranks.ts
+++ packages/server/src/util/ranks.ts
@@ -3,11 +3,12 @@ export const colorByRank = (
 ): { r: number; g: number; b: number } => {
   rank = rank.toLowerCase();
 
-  if (rank.includes('bronze')) return { r: 205, g: 127, b: 50 };
-  if (rank.includes('silver')) return { r: 192, g: 192, b: 192 };
-  if (rank.includes('gold')) return { r: 255, g: 215, b: 0 };
-  if (rank.includes('platinum')) return { r: 229, g: 228, b: 226 };
-  if (rank.includes('pro')) return { r: 255, g: 0, b: 0 };
+  if (rank.includes('iron')) return { r: 125, g: 43, b: 0 };
+  if (rank.includes('bronze')) return { r: 187, g: 119, b: 62 };
+  if (rank.includes('silver')) return { r: 167, g: 167, b: 167 };
+  if (rank.includes('gold')) return { r: 238, g: 175, b: 55 };
+  if (rank.includes('platinum')) return { r: 50, g: 204, b: 179 };
+  if (rank.includes('pro')) return { r: 200, g: 14, b: 14 };
 
   return { r: 255, g: 255, b: 255 };
 };
