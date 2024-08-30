e443d233e41d8fd493d0e80bb811bea23c899b04
Tripperful <tripperful@gmail.com>
2024-08-30 21:24:54 +0200

    Add cobalt rank color

--- packages/server/src/util/ranks.ts
+++ packages/server/src/util/ranks.ts
@@ -8,6 +8,7 @@ export const colorByRank = (
   if (rank.includes('silver')) return { r: 167, g: 167, b: 167 };
   if (rank.includes('gold')) return { r: 238, g: 175, b: 55 };
   if (rank.includes('platinum')) return { r: 50, g: 204, b: 179 };
+  if (rank.includes('cobalt')) return { r: 0, g: 124, b: 251 };
   if (rank.includes('pro')) return { r: 200, g: 14, b: 14 };
 
   return { r: 255, g: 255, b: 255 };
